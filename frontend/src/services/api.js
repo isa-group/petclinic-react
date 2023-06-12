6c9a639dadac672a7864ccf368a9698433ee06d7
Alex-GF <yalejandro9@gmail.com>
2023-06-12 18:57:03 +0200

    FEAT: INTERCEPTORS THAT UPDATE JWT TOKEN

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,55 +1,68 @@
 import axios from "axios";
 import TokenService from "./token.service";
 
-const instance = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
+// const instance = axios.create({
+//     baseURL: "http://localhost:8080/api/v1",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
 
-instance.interceptors.request.use(
-    (config) => {
-        const token = TokenService.getLocalAccessToken();
-        if (token) {
-            config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+// instance.interceptors.request.use(
+//     (config) => {
+//         const token = TokenService.getLocalAccessToken();
+//         if (token) {
+//             config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
 
-instance.interceptors.response.use(
-    (res) => {
-        return res;
-    },
-    async (err) => {
-        const originalConfig = err.config;
+// instance.interceptors.response.use(
+//     (res) => {
+//         return res;
+//     },
+//     async (err) => {
+//         const originalConfig = err.config;
 
-        if (originalConfig.url !== "/auth/signin" && err.response) {
-            // Access Token was expired
-            if (err.response.status === 401 && !originalConfig._retry) {
-                originalConfig._retry = true;
+//         if (originalConfig.url !== "/auth/signin" && err.response) {
+//             // Access Token was expired
+//             if (err.response.status === 401 && !originalConfig._retry) {
+//                 originalConfig._retry = true;
 
-                try {
-                    const rs = await instance.post("/auth/refreshtoken", {
-                        refreshToken: TokenService.getLocalRefreshToken(),
-                    });
+//                 try {
+//                     const rs = await instance.post("/auth/refreshtoken", {
+//                         refreshToken: TokenService.getLocalRefreshToken(),
+//                     });
 
-                    const { accessToken } = rs.data;
-                    TokenService.updateLocalAccessToken(accessToken);
+//                     const { accessToken } = rs.data;
+//                     TokenService.updateLocalAccessToken(accessToken);
 
-                    return instance(originalConfig);
-                } catch (_error) {
-                    return Promise.reject(_error);
-                }
-            }
-        }
+//                     return instance(originalConfig);
+//                 } catch (_error) {
+//                     return Promise.reject(_error);
+//                 }
+//             }
+//         }
 
-        return Promise.reject(err);
-    }
-);
+//         return Promise.reject(err);
+//     }
+// );
 
-export default instance;
+// export default instance;
+
+export function fetchWithInterceptor(url, options) {
+    return fetch(url, options).then((response) => {
+      // Check if the response contains the 'newToken' header and update the token in localStorage
+      const newToken = response.headers.get('newToken');
+  
+      if (newToken && newToken !== 'null') {
+        localStorage.setItem('token', newToken);
+      }
+  
+      return response;
+    });
+  }
