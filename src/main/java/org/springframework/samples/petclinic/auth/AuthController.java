34a89f691786c511d201d4067688854e96849008
Alex-GF <yalejandro9@gmail.com>
2023-06-15 11:14:57 +0200

    FEAT: USING PROPERTIES SECRET FOR TOKEN CREATION

--- src/main/java/org/springframework/samples/petclinic/auth/AuthController.java
+++ src/main/java/org/springframework/samples/petclinic/auth/AuthController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import petclinic.payload.request.LoginRequest;
 import petclinic.payload.request.SignupRequest;
